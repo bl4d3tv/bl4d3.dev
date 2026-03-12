1fd3b05434a00fdf358221d3f1561ba6a7409820
bl4d3tv <me@bl4d3.dev>
2026-03-12 14:12:38 -0300

    refactor: migrate to astro's font api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -11,6 +11,14 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   site: "https://bl4d3.dev",
   trailingSlash: "never",
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Uncut Sans",
+      cssVariable: "--font-uncut-sans",
+    },
+  ],
 
   vite: {
     plugins: [tailwindcss()],
